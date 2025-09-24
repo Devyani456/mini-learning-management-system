a9f36aa029fd4f2e2beb37b6ae49fc04cf855d95
Devyani <195336326+Devyani456@users.noreply.github.com>
2025-09-24 19:37:58 +0530

    Utils.java

--- Utils.java
+++ Utils.java
@@ -1,12 +1,58 @@
 package util;
 
-import java.util.Scanner;
+import models.CourseModel;
+import models.User;
 
 public class Utils {
-    public static Scanner scanner = new Scanner(System.in);
+    public static User[] users = new User[100];
+    public static int userCount = 0;
 
-    public static String input(String message) {
-        System.out.print(message + ": ");
-        return scanner.nextLine();
+    public static CourseModel[] courses = new CourseModel[100];
+    public static int courseCount = 0;
+
+    // helper: generate simple IDs
+    private static int uid = 1;
+    private static int cid = 1;
+
+    public static String generateUserId() {
+        return "U" + (uid++);
+    }
+
+    public static String generateCourseId() {
+        return "C" + (cid++);
+    }
+
+    // to add user
+    public static void addUser(User u) {
+        users[userCount++] = u;
+    }
+
+    // add course 
+    public static void addCourse(CourseModel c) {
+        courses[courseCount++] = c;
+    }
+
+    // to find user by email
+    public static User findUserByEmail(String email) {
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].email.equals(email)) return users[i];
+        }
+        return null;
+    }
+
+    // find user by id
+    public static User findUserById(String id) {
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].userId.equals(id)) return users[i];
+        }
+        return null;
+    }
+
+    // find course by id
+    public static CourseModel findCourseById(String id) {
+        for (int i = 0; i < courseCount; i++) {
+            if (courses[i].courseId.equals(id)) return courses[i];
+        }
+        return null;
     }
 }
