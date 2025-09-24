8a4844b8506a1d1eec5de58d902b4d23a0b4b0fc
Devyani <195336326+Devyani456@users.noreply.github.com>
2025-09-24 19:31:07 +0530

    CourseModel.java

--- CourseModel.java
+++ CourseModel.java
@@ -1,18 +1,13 @@
 package models;
 
 public class CourseModel {
-    private int courseId;
-    private String courseName;
-    private String courseDuration;
+    public String courseId;
+    public String courseName;
+    public String courseDuration;
 
-    public CourseModel(int courseId, String courseName, String courseDuration) {
+    public CourseModel(String courseId, String courseName, String courseDuration) {
         this.courseId = courseId;
         this.courseName = courseName;
         this.courseDuration = courseDuration;
     }
-
-    // Getters
-    public int getCourseId() { return courseId; }
-    public String getCourseName() { return courseName; }
-    public String getCourseDuration() { return courseDuration; }
 }
